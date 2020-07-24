b94ffd270ca5b6ee5d0bce0a824719c3c49de771
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-07-24 15:29:28 -0300

    Remove methods and observers

--- component/component.js
+++ component/component.js
@@ -61,34 +61,6 @@ export default Ember.Component.extend(ClusterDriver, {
     editing: equal('mode', 'edit'),
     config: alias('cluster.%%DRIVERNAME%%EngineConfig'),
 
-    parseCloudProviderVersionChoices(versions, providerVersion, mode) {
-        let {
-            intl,
-            defaultK8sVersionRange
-        } = this;
-        const maxVersionRange = defaultK8sVersionRange.split(' ').pop();
-
-        return versions.map((version) => {
-            if (satisfies(coerceVersion(version), maxVersionRange)) {
-                const out = {
-                    label: version,
-                    value: version,
-                };
-
-                if (mode === 'edit') {
-                    if (Semver.lt(coerceVersion(version), coerceVersion(providerVersion))) {
-                        setProperties(out, {
-                            disabled: true,
-                            label: `${out.label} ${intl.t('formVersions.downgrade')}`
-                        });
-                    }
-                }
-
-                return out;
-            }
-        });
-    },
-
     init() {
         /*!!!!!!!!!!!DO NOT CHANGE START!!!!!!!!!!!*/
         // This does on the fly template compiling, if you mess with this :cry:
@@ -157,26 +129,7 @@ export default Ember.Component.extend(ClusterDriver, {
     config: alias('cluster.%%DRIVERNAME%%EngineConfig'),
 
     actions: {
-        multiSecurityGroupSelect() {
-            let options = Array.prototype.slice.call($('.existing-security-groups')[0], 0);
-            let selectedOptions = [];
 
-            options.filterBy('selected', true).forEach((cap) => {
-                return selectedOptions.push(cap.value);
-            });
-
-            set(this, 'cluster.%%DRIVERNAME%%EngineConfig.securityGroups', selectedOptions);
-        },
-        multiSubnetGroupSelect() {
-            let options = Array.prototype.slice.call($('.existing-subnet-groups')[0], 0);
-            let selectedOptions = [];
-
-            options.filterBy('selected', true).forEach((cap) => {
-                return selectedOptions.push(cap.value);
-            });
-
-            set(this, 'cluster.%%DRIVERNAME%%EngineConfig.subnets', selectedOptions);
-        },
         awsLogin(cb) {
             set(this, 'step', 2)
             cb()
@@ -184,31 +137,6 @@ export default Ember.Component.extend(ClusterDriver, {
         loadVPS(cb) {
             set(this, 'step', 3)
             cb()
-        },
-        setVPCS(cb) {
-            if (get(this, 'vpcSubnetMode') === 'custom') {
-                this.loadSubnets(this.authCreds()).then(() => {
-                    set(this, 'step', 4);
-
-                    cb();
-                }).catch((err) => {
-                    get(this, 'errors').pushObject(err);
-                    cb(false, err);
-                });
-            } else {
-                set(this, 'step', 6);
-            }
-        },
-
-        setSubnets(cb) {
-            this.loadSecurityGroups(this.authCreds()).then(() => {
-                set(this, 'step', 5);
-
-                cb();
-            }).catch((err) => {
-                get(this, 'errors').pushObject(err);
-                cb(false, err);
-            });
         },
 
         save(cb) {
@@ -235,12 +163,6 @@ export default Ember.Component.extend(ClusterDriver, {
         },
     },
 
-    publicIpChanged: observer('step', 'cluster.%%DRIVERNAME%%EngineConfig.associateWorkerNodePublicIp', function () {
-        if (!get(this, 'cluster.%%DRIVERNAME%%EngineConfig.associateWorkerNodePublicIp')) {
-            set(this, 'vpcSubnetMode', 'custom');
-        }
-    }),
-
     desiredNodesChanged: observer('cluster.%%DRIVERNAME%%EngineConfig.desiredNodes', function () {
         const desiredNodes = get(this, 'cluster.%%DRIVERNAME%%EngineConfig.desiredNodes');
         const config = get(this, 'cluster.%%DRIVERNAME%%EngineConfig');
@@ -251,65 +173,6 @@ export default Ember.Component.extend(ClusterDriver, {
         })
     }),
 
-    vpcSubnetModeDidChange: observer('step', 'vpcSubnetMode', function () {
-        const {
-            vpcSubnetMode: mode,
-            editing,
-            step,
-        } = this;
-
-        if (!editing && mode === 'custom') {
-            const vpc = get(this, 'filteredVpcs.firstObject.id');
-
-            if (step <= 3) {
-                if (vpc) {
-                    set(this, 'cluster.%%DRIVERNAME%%EngineConfig.virtualNetwork', vpc);
-                }
-            }
-        }
-    }),
-
-    serviceRoleModeDidChange: observer('serviceRoleMode', function () {
-        const mode = get(this, 'serviceRoleMode');
-
-        if (mode === 'custom') {
-            const role = get(this, 'serviceRoles.firstObject.RoleName');
-
-            if (role) {
-                set(this, 'selectedServiceRole', role);
-            }
-        } else {
-            set(this, 'selectedServiceRole', null);
-        }
-    }),
-
-    vpcsChanged: observer('cluster.%%DRIVERNAME%%EngineConfig.virtualNetwork', 'vpcSubnetMode', function () {
-        if (!this.primaryResource.isTransitioning) {
-            const vnet = get(this, 'cluster.%%DRIVERNAME%%EngineConfig.virtualNetwork');
-            const subnets = get(this, 'cluster.%%DRIVERNAME%%EngineConfig.subnets');
-            const mode = get(this, 'vpcSubnetMode');
-            const hasInitializedValues = vnet || subnets;
-
-            if (vnet && mode === 'custom') {
-                this.loadSubnets(this.authCreds()).catch((err) => {
-                    get(this, 'errors').pushObject(err);
-                });
-                // We check for initialized values here because as part of
-                // the saving process this observer gets triggered with
-                // uninitialized values. This was causing a save to switch
-                // the step to step 3 rather than remaining on the last
-                // page until the saving was complete.
-            } else if (mode === 'default' && hasInitializedValues) {
-                setProperties(get(this, 'cluster.%%DRIVERNAME%%EngineConfig'), {
-                    virtualNetwork: null,
-                    subnets: [],
-                });
-
-                set(this, 'step', 3);
-            }
-        }
-    }),
-
     versionChoices: computed('versions', function () {
         let versionChoices = [
             { label: "aaa", value: "aaa" }
@@ -317,228 +180,6 @@ export default Ember.Component.extend(ClusterDriver, {
 
         return versionChoices;
     }),
-
-    filteredKeyPairs: computed('allKeyPairs', function () {
-        return get(this, 'allKeyPairs').sortBy('KeyName');
-    }),
-
-    filteredSubnets: computed('allSubnets', function () {
-        return get(this, 'allSubnets').filterBy('VpcId', get(this, 'cluster.%%DRIVERNAME%%EngineConfig.virtualNetwork')).map((subnet) => {
-            return {
-                subnetName: nameFromResource(subnet, 'SubnetId'),
-                subnetId: subnet.SubnetId,
-            }
-        }).sortBy('subnetName');
-    }),
-
-    filteredVpcs: computed('allVpcs', function () {
-        return (get(this, 'allVpcs') || []).filterBy('State', 'available').map((vpc) => {
-            return {
-                id: get(vpc, 'VpcId'),
-                label: `${get(vpc, 'VpcId')} (${get(vpc, 'CidrBlock')})`
-            };
-        }).sortBy('label');
-    }),
-
-    filteredSecurityGroups: computed('allSecurityGroups', function () {
-        return get(this, 'allSecurityGroups').filterBy('VpcId', get(this, 'cluster.%%DRIVERNAME%%EngineConfig.virtualNetwork')).sortBy('GroupName');
-    }),
-
-    readableServiceRole: computed('cluster.%%DRIVERNAME%%EngineConfig.serviceRole', function () {
-        const roles = get(this, 'serviceRoles');
-        const selectedRole = get(this, 'cluster.%%DRIVERNAME%%EngineConfig.serviceRole');
-        const match = roles.findBy('RoleName', selectedRole);
-
-        return match && match.RoleName ? get(match, 'RoleName') : this.app.t('nodeDriver.amazoneks.role.noneSelected');
-    }),
-
-    canSaveVPC: computed('vpcSubnetMode', 'selectedGroupedDetails', 'cluster.%%DRIVERNAME%%EngineConfig.virtualNetwork', 'cluster.%%DRIVERNAME%%EngineConfig.subnets.[]', function () {
-        const mode = get(this, 'vpcSubnetMode');
-        const config = get(this, 'cluster.%%DRIVERNAME%%EngineConfig');
-        let disabled = true;
-
-        if (mode === 'default' || get(config, 'virtualNetwork')) {
-            disabled = false;
-        }
-
-        return disabled;
-    }),
-
-    canSaveSG: computed('cluster.%%DRIVERNAME%%EngineConfig.securityGroups.[]', function () {
-        const sg = get(this, 'cluster.%%DRIVERNAME%%EngineConfig.securityGroups');
-
-        let disabled = true;
-
-        if (sg && sg.length > 0) {
-            disabled = false;
-        }
-
-        return disabled;
-    }),
-
-    authCreds() {
-        const config = get(this, 'cluster.%%DRIVERNAME%%EngineConfig');
-
-        let {
-            accessKey, secretKey, region, sessionToken
-        } = config;
-
-        console.log(accessKey, secretKey, region, sessionToken)
-
-        accessKey = (accessKey || '').trim();
-        secretKey = (secretKey || '').trim();
-
-        setProperties(this, {
-            'errors': [],
-            'cluster.%%DRIVERNAME%%EngineConfig.accessKey': accessKey,
-            'cluster.%%DRIVERNAME%%EngineConfig.secretKey': secretKey,
-        });
-
-        const auth = {
-            region,
-            accessKeyId: accessKey,
-            secretAccessKey: secretKey,
-        };
-
-        if (sessionToken) {
-            let token = sessionToken.trim();
-
-            set(auth, 'sessionToken', token);
-            set(this, 'cluster.%%DRIVERNAME%%EngineConfig.sessionToken', token);
-        }
-
-        return auth;
-    },
-
-    loadVpcs(auth) {
-        return this.listVPCs(auth).then((resp) => {
-            let { vpcs } = resp;
-
-            let def = vpcs.findBy('IsDefault');
-
-            if (def && def.VpcId) {
-                set(this, 'cluster.%%DRIVERNAME%%EngineConfig.virtualNetwork', get(def, 'VpcId'));
-            }
-
-            return set(this, 'allVpcs', vpcs);
-        });
-    },
-
-    loadSubnets(auth) {
-        return this.listSubnets(auth).then((resp) => {
-            return set(this, 'allSubnets', resp);
-        });
-    },
-
-    loadSecurityGroups(auth) {
-        return this.listSecurityGroups(auth).then((resp) => {
-            return set(this, 'allSecurityGroups', resp);
-        });
-    },
-
-    loadKeyPairs(auth) {
-        return this.listKeyPairs(auth).then((resp) => {
-            return set(this, 'allKeyPairs', resp);
-        });
-    },
-
-    listKeyPairs(auth) {
-        return new Promise((resolve, reject) => {
-            const ec2 = new AWS.EC2(auth);
-
-            ec2.describeKeyPairs({}, (err, data) => {
-                if (err) {
-                    console.log(err, err.stack);
-                    reject(err);
-                }
-
-                resolve(data.KeyPairs);
-            });
-        })
-    },
-
-    listRoles(auth) {
-        // TODO There is no IAM endpoint in cn-northwest-1 region. We need to use cn-north-1 for now. So users chould be able to create EKS cluster in cn-northwest-1.
-
-        const { region } = auth || {};
-
-        if (region === 'cn-northwest-1') {
-            auth.region = 'cn-north-1';
-        }
-
-        return new Promise((resolve, reject) => {
-            const IAM = new AWS.IAM(auth);
-
-            IAM.listRoles({}, (err, data) => {
-                if (err) {
-                    console.log(err, err.stack);
-                    reject(err);
-                }
-
-                resolve(data.Roles);
-            });
-        });
-    },
-
-    listVPCs(auth) {
-        return new Promise((resolve, reject) => {
-            const ec2 = new AWS.EC2(auth);
-            const vpcNames = {};
-            const vpcTags = {};
-
-            ec2.describeVpcs({}, (err, vpcs) => {
-                if (err) {
-                    return reject(err);
-                }
-
-                vpcs.Vpcs.forEach((vpc) => {
-                    vpcNames[vpc.VpcId] = nameFromResource(vpc, 'VpcId');
-                    vpcTags[vpc.VpcId] = tagsFromResource(vpc);
-                });
-
-                return resolve({
-                    vpcNames,
-                    vpcTags,
-                    vpcs: vpcs.Vpcs
-                });
-            });
-        });
-    },
-
-    listSubnets(auth) {
-        const ec2 = new AWS.EC2(auth);
-        const rName = get(this, 'cluster.%%DRIVERNAME%%EngineConfig.region');
-        let subnets = [];
-
-
-        return new Promise((resolve, reject) => {
-            ec2.describeSubnets({}, (err, data) => {
-                if (err) {
-                    reject(err)
-                }
-
-                set(this, `clients.${rName}`, ec2)
-
-                subnets = data.Subnets;
-
-                resolve(subnets);
-            });
-        });
-    },
-
-    listSecurityGroups(auth) {
-        const ec2 = new AWS.EC2(auth);
-
-        return new Promise((resolve, reject) => {
-            ec2.describeSecurityGroups({}, (err, data) => {
-                if (err) {
-                    reject(err)
-                }
-
-                resolve(data.SecurityGroups);
-            });
-        });
-    },
 
     willSave() {
         // temporary measure put in place for rancher/rancher#24652
